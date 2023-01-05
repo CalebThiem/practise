ef868290f51a883a4ded99388f45d472b8373a5f
Caleb <calebthiem@protonmail.com>
2023-01-05 19:48:16 +0200

    Fininshed project

--- credit.c
+++ credit.c
@@ -1,3 +1,4 @@
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
@@ -9,6 +10,14 @@
 #define MAX_ARRAY_LENGTH MAX_CARD_LENGTH + 2
 
 int newLineSearch(char * userInput);
+
+int checkInput(char * userInput, int inputLength);
+
+int checkLuhn(char * userInput, int inputLength);
+
+int addDigits(char * digitList, int multiply);
+
+int identifyCard(char * userInput, int inputLength);
 
 int numberValid = 0; 
 
@@ -26,6 +35,35 @@ int main(void)
 
         if (newLineSearch(userInput) == 1)
         {
+            int inputLength = strlen(userInput);
+
+            if (checkInput(userInput, inputLength) == 1)
+            {
+                if (checkLuhn(userInput, inputLength) == 1)
+                {       
+                    numberValid = 1; 
+
+                    switch (identifyCard(userInput, inputLength)) {
+                        case 1: 
+                            printf("VISA\n");
+                            break;
+                        case 2:
+                            printf("MASTERCARD\n");
+                            break;
+                        case 3:
+                            printf("AMEX\n");
+                            break; 
+                        case 0:
+                            printf("INVALID\n");
+                            break;
+                    }
+                }
+                else {
+                    printf("INVALID\n");
+                    break;
+                }
+
+            }
             /* Write number checking function **TO DO**
             if (checkNumber == 1)
                 numberValid = 1;           
@@ -63,4 +101,161 @@ int newLineSearch(char * userInput)
     }
 
     return 0;
+}
+
+int checkInput(char * userInput, int inputLength)
+{
+    for (int i = 0; i < inputLength - 1; i++)
+    {
+        if (isdigit(userInput[i]) == 0)
+        { 
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+int checkLuhn(char * userInput, int inputLength)
+{
+    // Ignore newline character at end of string
+
+    inputLength--;
+
+    // Copy every other character to evens, copy the rest to odds
+    
+    char evens[(inputLength % 2) + 1];
+
+    char odds[(inputLength % 2) + 1];
+
+        int i = 0;
+        int j = 0;
+
+        for (; i <= inputLength - 2; i = i + 2)
+        {
+            evens[j] = userInput[(i + 1)];
+
+            j++;
+        }
+
+        // End the string with a NUL byte
+
+        evens[j++] = '\0';
+        
+        i = 0;  
+        j = 0;
+
+        for (; i <= inputLength - 1; i = i + 2)
+        {
+            odds[j] = userInput[i];
+            j++;
+        }
+        
+        odds[j] = '\0';
+
+        int luhn = 0;
+
+        if (inputLength % 2 == 0)
+        {
+            luhn = addDigits(odds, 1);
+            luhn = luhn + addDigits(evens, 0);
+        }
+        else {
+            luhn = addDigits(evens, 1);
+            luhn = luhn + addDigits(odds, 0);
+        }
+
+    if (luhn % 10 == 0) 
+    {
+        return 1;
+    } else {
+        return 0;
+    }
+
+}
+
+int addDigits(char * digitList, int multiply)
+{
+    int inputLength = strlen(digitList);
+
+    char stepOneString[inputLength];
+
+    stepOneString[0] = '\0';
+
+    char numbers;
+
+    int evensLen = strlen(digitList);
+
+    for (int k = 0; k < evensLen; k++)
+    {
+        int tempInt = 0;
+
+        tempInt = digitList[k] - '0';
+
+        if (multiply == 1)
+        {
+            tempInt = tempInt * 2;
+        }
+
+        char tempString[3];
+
+        sprintf(tempString, "%d", tempInt);
+
+        strcat(stepOneString, tempString);
+    } 
+
+    int stepOneStringLen = strlen(stepOneString); 
+
+    int stepTwoInt = 0;
+
+    for (int k = 0; k < stepOneStringLen; k++) 
+    {
+        stepTwoInt = stepTwoInt + (stepOneString[k] - '0');
+    }
+
+    return stepTwoInt;
+}
+
+int identifyCard(char * userInput, int inputLength)
+{
+    if (userInput[0] == '4' && (((inputLength - 1) == 13) || ((inputLength - 1) == 16)))
+    {
+        return 1;
+    }
+
+    if (userInput[0] == '5' && ((inputLength - 1) == 16))
+    {
+        switch (userInput[1]) {
+            case '1':
+                return 2;
+                break;
+            case '2':
+                return 2;
+                break;
+            case '3':
+                return 2;
+                break;
+            case '4':
+                return 2;
+                break;
+            case '5':
+                return 2;
+                break;
+        }
+    }
+
+    if (userInput[0] == '3' && ((inputLength - 1) == 15))
+    {
+        switch (userInput[1]) {
+            case '4':
+                return 3;
+                break;
+            case '7':
+                return 3;
+                break;
+        }
+    }
+
+    return 0;
+        
 }
